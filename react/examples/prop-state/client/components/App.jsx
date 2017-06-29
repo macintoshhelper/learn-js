948f882e063f836e1269d492883e3a8feb82ecf8
Macintosh Helper <noreply@macintoshhelper.com>
2017-06-29 20:00:56 +0100

    Create React example of using prop and state from input

---
+++ react/examples/prop-state/client/components/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div style={{ textAlign: 'center' }}>
+//         <h1>Hello World</h1>
+//       </div>);
+//   }
+// }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: 'Text from state',
+    };
+  }
+
+  render() {
+    const text = this.state.text;
+
+    return (
+      <div>
+        <h2>{text || this.props.text }</h2>
+        <input onChange={event => (this.setState({ text: event.target.value }))} />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+App.defaultProps = {
+  text: 'Default text',
+};
+
+export default App;
