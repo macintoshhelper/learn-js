e72f9ad392a143ba44e9b274dd3a9cedd269ed0f
Macintosh Helper <noreply@macintoshhelper.com>
2017-07-04 15:02:29 +0100

    Adding-to-dom ReactJS example project

---
+++ react/examples/adding-to-dom/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const sourcePath = path.join(__dirname, './client');
+const jsSourcePath = path.join(sourcePath, './js');
+const buildPath = path.join(__dirname, './build');
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: path.join(sourcePath, 'index.html'),
+    path: buildPath,
+    filename: 'index.html',
+  }),
+];
+
+const rules = [
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: [
+      'babel-loader',
+    ],
+  },
+];
+
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    path.join(jsSourcePath, 'index.js'),
+  ],
+  output: {
+    path: buildPath,
+    publicPath: '/',
+    filename: 'app-[hash].js',
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      jsSourcePath,
+    ],
+  },
+  module: {
+    rules,
+  },
+  plugins,
+};
